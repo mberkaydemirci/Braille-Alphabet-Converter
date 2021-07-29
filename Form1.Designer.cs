fc82291378e3a65149bf6fba8f41172d44744edd
mehmetberkaydemirci <67818593+mberkaydemirci@users.noreply.github.com>
2021-07-29 04:39:12 +0300

    Braille-Alphabet-Converter

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Görsel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.çeviriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.çeviriToolStripMenuItem,
+            this.hakkındaToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // çeviriToolStripMenuItem
+            // 
+            this.çeviriToolStripMenuItem.Checked = true;
+            this.çeviriToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.çeviriToolStripMenuItem.Name = "çeviriToolStripMenuItem";
+            this.çeviriToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.çeviriToolStripMenuItem.Text = "Çeviri";
+            this.çeviriToolStripMenuItem.Click += new System.EventHandler(this.çeviriToolStripMenuItem_Click);
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            this.hakkındaToolStripMenuItem.Click += new System.EventHandler(this.hakkındaToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Metin Giriniz:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Çevir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 311);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Braille Alfabe Çevirici";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem çeviriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Görsel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            String a = (textBox1.Text);
+            String[] alfabe = { " ","\u2801", "\u2803","\u2809", "\u2821", "\u2819", "\u2821", "\u280B", "\u281B","\u2823","\u2813", "\u2814", "\u280A", "\u281A", "\u2805", "\u2838", "\u280D", "\u281D", "\u2815", "\u282A", "\u280F", "\u281F", "\u2817", "\u280E", "\u2829", "\u281E", "\u2825", "\u2833", "\u2827", "\u283A", "\u282D", "\u283D", "\u2835","\u28A4", "\u2802", "\u2844", "\u2812", "\u2826", "\u2816", "\u28C0", "\u2830 \u28C4","\u28A0 \u2812","\u2830 \u2884","\u2830 \u28C0","\u2830 \u2836","\u28A4 \u28A4 \u28A4", "\u2826 \u2834", "\u2836", "\u2836", "\u2808","\u2840" };
+            String[] harf = { " ","a", "b", "c", "ç", "d", "e", "f", "g", "ğ", "h", "ı", "i", "j", "k", "l", "m", "n", "o", "ö", "p", "q", "r", "s", "ş", "t", "u", "ü", "v", "w", "x", "y", "z",".",",",";",":","?","!","-","*","/","+","-","=","...",@"""","(",")","^","'"};
+            String[] bharf = { " ", "A", "B", "C", "Ç", "D", "E", "F", "G", "Ğ", "H", "I", "İ", "J", "K", "L", "M", "N", "O", "Ö", "P", "Q", "R", "S", "Ş", "T", "U", "Ü", "V", "W", "X", "Y", "Z",};
+            String[] rakam = { "1", "2", "3", "4", "5", "6", "7", "8", "9","0" };
+            string[] irakam = { "\u2801", "\u2803", "\u2809", "\u2819", "\u2821", "\u280B", "\u281B", "\u2813", "\u280A", "\u281A" };
+            string[] sayisa = { "\u283C", "\u28C0 \u28C0" };
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] >= 'A' && a[i] <= 'Z')
+                {
+                    label2.Text += "\u2820";
+                }
+                for (int j = 0; j < alfabe.Length; j++)
+                {
+                    if (Convert.ToString(a[i]) == harf[j])
+                    {
+                        label2.Text += alfabe[j];
+                    }
+
+                }
+                for (int j = 0; j < irakam.Length; j++)
+                {
+                    if (Convert.ToString(a[i]) == rakam[j])
+                       
+                        label2.Text += irakam[j];
+                }
+                for (int j = 0; j <bharf.Length; j++)
+                {
+                    if (Convert.ToString(a[i]) == bharf[j])
+                    {                      
+                        label2.Text += alfabe[j];
+                    }
+                        
+                }
+            }
+
+        }
+
+        private void hakkındaToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void çeviriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.ShowDialog();
+
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+                
+                   
+                
+            }
+                
+            
+    
+            
+
